83377f63c62d8927886d4814387304b777bfcb3f
LakshithaCS <e17338@eng.pdn.ac.lk>
2025-07-12 18:59:04 +0530

    Integration Tests with consumer

--- kafka-producer/src/test/java/com/springboot/kafka/controller/LibraryEventsControllerIntegrationTest.java
+++ kafka-producer/src/test/java/com/springboot/kafka/controller/LibraryEventsControllerIntegrationTest.java
@@ -1,8 +1,16 @@
 package com.springboot.kafka.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.springboot.kafka.dto.LibraryEvent;
 import com.springboot.kafka.util.TestUtil;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,8 +21,14 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.context.TestPropertySource;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @EmbeddedKafka(topics = "library-events")
@@ -24,6 +38,31 @@ class LibraryEventsControllerIntegrationTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
+
+    private static EmbeddedKafkaBroker broker;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private Consumer<Integer, String> consumer;
+
+    @BeforeEach
+    void setUp(@Autowired EmbeddedKafkaBroker embeddedKafkaBroker) {
+        broker = embeddedKafkaBroker;
+        consumer = new DefaultKafkaConsumerFactory<>(
+                getConfigs(),
+                new IntegerDeserializer(),
+                new StringDeserializer()
+        ).createConsumer();
+        broker.consumeFromAllEmbeddedTopics(consumer);
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (consumer != null) {
+            consumer.close();
+        }
+    }
 
     @Test
     void testPostLibraryEvent() {
@@ -43,6 +82,30 @@ class LibraryEventsControllerIntegrationTest {
         // then
         Assertions.assertEquals(HttpStatus.CREATED, response.getStatusCode());
 
+        ConsumerRecords<Integer, String> records = KafkaTestUtils.getRecords(consumer);
+        assert records.count() == 1;
+
+        records.forEach(
+                record -> {
+                    try {
+                        LibraryEvent libraryEvent = objectMapper.readValue(record.value(), LibraryEvent.class);
+                        Assertions.assertEquals(TestUtil.newLibraryEventRecordWithLibraryEventId().libraryEventId(), libraryEvent.libraryEventId());
+                        Assertions.assertEquals(TestUtil.newLibraryEventRecordWithLibraryEventId().book(), libraryEvent.book());
+                    } catch (Exception e) {
+                        Assertions.fail("Failed to parse LibraryEvent from record", e);
+                    }
+                }
+        );
+
+    }
+
+
+    private Map<String, Object> getConfigs() {
+        Map<String, Object> configs = new HashMap<>(
+                KafkaTestUtils.consumerProps("group1", "true", broker)
+        );
+        configs.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        return configs;
     }
 
 }
