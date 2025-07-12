b960d89c5d29890a83ace4c1a096f761cf62594e
LakshithaCS <e17338@eng.pdn.ac.lk>
2025-07-12 20:14:20 +0530

    put endpoint for updating library events

--- kafka-producer/src/main/java/com/springboot/kafka/controller/LibraryEventsController.java
+++ kafka-producer/src/main/java/com/springboot/kafka/controller/LibraryEventsController.java
@@ -2,11 +2,13 @@ package com.springboot.kafka.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.springboot.kafka.dto.LibraryEvent;
+import com.springboot.kafka.dto.LibraryEventType;
 import com.springboot.kafka.producer.LibraryEventsProducer;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,5 +33,27 @@ public class LibraryEventsController {
         libraryEventsProducer.sendLibraryEventSynchronous(libraryEvent);
 
         return ResponseEntity.status(CREATED).body(libraryEvent);
+    }
+
+    @PutMapping("/v1/libraryevent")
+    public ResponseEntity<?> putLibraryEvent(@RequestBody @Valid LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException {
+        ResponseEntity<String> response = validationRequest(libraryEvent);
+        if (response != null) return response;
+
+        log.info("LibraryEvent: {}", libraryEvent);
+        libraryEventsProducer.sendLibraryEventSynchronous(libraryEvent);
+
+        return ResponseEntity.status(CREATED).body(libraryEvent);
+    }
+
+    private ResponseEntity<String> validationRequest(LibraryEvent libraryEvent) {
+        if (libraryEvent.libraryEventId() == null) {
+            return ResponseEntity.badRequest().body("LibraryEvent ID cannot be null for update operation");
+        }
+
+        if (!libraryEvent.libraryEventType().equals(LibraryEventType.UPDATE)) {
+            return ResponseEntity.badRequest().body("LibraryEvent type must be UPDATE for update operation");
+        }
+        return null;
     }
 }
