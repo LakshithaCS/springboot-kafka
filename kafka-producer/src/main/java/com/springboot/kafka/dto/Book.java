005dd4629145a4d771e1726e90f71e87a7f6cd09
LakshithaCS <e17338@eng.pdn.ac.lk>
2025-07-12 19:41:04 +0530

    Add field validations in dtos and controller

--- kafka-producer/src/main/java/com/springboot/kafka/dto/Book.java
+++ kafka-producer/src/main/java/com/springboot/kafka/dto/Book.java
@@ -1,8 +1,14 @@
 package com.springboot.kafka.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record Book(
+        @NotNull
         Integer bookId,
+        @NotBlank
         String bookName,
+        @NotBlank
         String bookAuthor
 ) {
 }
--- kafka-producer/src/main/java/com/springboot/kafka/dto/LibraryEvent.java
+++ kafka-producer/src/main/java/com/springboot/kafka/dto/LibraryEvent.java
@@ -1,8 +1,13 @@
 package com.springboot.kafka.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+
 public record LibraryEvent(
         Integer libraryEventId,
         LibraryEventType libraryEventType,
+        @NotNull
+        @Valid
         Book book
 ) {
 }
