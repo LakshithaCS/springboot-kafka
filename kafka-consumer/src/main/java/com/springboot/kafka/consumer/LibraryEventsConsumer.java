f2bacebd7686691ea6581bdedd2d932deca0be8e
LakshithaCS <e17338@eng.pdn.ac.lk>
2025-07-19 18:25:53 +0530

    Consumer full implementation

--- kafka-consumer/src/main/java/com/springboot/kafka/consumer/LibraryEventsConsumer.java
+++ kafka-consumer/src/main/java/com/springboot/kafka/consumer/LibraryEventsConsumer.java
@@ -1,5 +1,7 @@
 package com.springboot.kafka.consumer;
 
+import com.springboot.kafka.service.LibraryEventsService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -7,17 +9,20 @@ import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class LibraryEventsConsumer {
+
+    private final LibraryEventsService libraryEventsService;
 
     @KafkaListener(topics = "${spring.kafka.topic}", groupId = "${spring.kafka.consumer.group-id}")
     public void onLibraryEvent(ConsumerRecord<Integer, String> message) {
 
         log.info("Received message: {}", message);
-
-        Integer key = message.key();
-        String value = message.value();
-
-        log.info("Key: {}, Value: {}", key, value);
+        try {
+            libraryEventsService.processLibraryEvent(message);
+        } catch (Exception e) {
+            log.error("Error processing message: {}", e.getMessage(), e);
+        }
 
     }
 
