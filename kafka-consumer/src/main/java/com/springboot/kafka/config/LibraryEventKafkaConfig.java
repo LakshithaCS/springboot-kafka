aa244f1aa86460add8b454a0e6e8b6f9f9b2c3d6
LakshithaCS <e17338@eng.pdn.ac.lk>
2025-07-20 15:05:40 +0530

    Retry handling

--- kafka-consumer/src/main/java/com/springboot/kafka/config/LibraryEventKafkaConfig.java
+++ kafka-consumer/src/main/java/com/springboot/kafka/config/LibraryEventKafkaConfig.java
@@ -6,7 +6,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.CommonErrorHandler;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
+
+import java.util.List;
 
 @Configuration
 @EnableKafka
@@ -20,8 +24,25 @@ public class LibraryEventKafkaConfig {
         ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
         configurer.configure(factory, kafkaConsumerFactory);
         factory.setConcurrency(3); // Set the concurrency level for parallel processing
+        factory.setCommonErrorHandler(getErrorHandler()); // Set a common error handler if needed
         // factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
         return factory;
+    }
+
+    private CommonErrorHandler getErrorHandler() {
+        FixedBackOff backOff = new FixedBackOff(1000L, 3); // Retry 3 times with a 1 second interval
+        var exceptionToIgnore = List.of(IllegalArgumentException.class);
+
+        DefaultErrorHandler errorHandler = new DefaultErrorHandler(backOff);
+
+        errorHandler.setRetryListeners(
+                (record, exception, deliveryAttempt) -> {
+                    System.out.println("Retrying record: " + record + ", attempt: " + deliveryAttempt);
+                }
+        );
+        exceptionToIgnore.forEach(errorHandler::addRetryableExceptions);
+
+        return errorHandler;
     }
 
 }
